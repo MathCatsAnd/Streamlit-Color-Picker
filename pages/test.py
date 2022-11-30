339426ef13e48fb32d06bc1c92b8589645432f06
MathCatsAnd <mathcatsand@gmail.com>
2022-11-29 18:51:31 -0800

    File path test
    Testing Streamlit Cloud's handling of relative paths.

---
+++ pages/test.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import pandas as pd
+
+
+try:
+    st.write('Trying with back slashes')
+    st.dataframe(pd.read_csv(r'.\\files\\test.csv'))
+except:
+    st.write('It didn\'t work with back slashes.')
+
+
+try:
+    st.write('Trying with forward slashes')
+    st.dataframe(pd.read_csv(r'files/test.csv'))
+except:
+    st.write('It didn\'t work with forward slashes.')
